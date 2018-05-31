67a384a9b95206ece45b10342601faa077109bce
Igor Kulman <igor@kulman.sk>
2018-05-31 09:31:24 +0200

    ♻️ cleanup

--- LocalizationEditor/AppDelegate.swift
+++ LocalizationEditor/AppDelegate.swift
@@ -6,8 +6,8 @@
 //  Copyright © 2018 Teamwire. All rights reserved.
 //
 
-import Cocoa
 import CleanroomLogger
+import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
@@ -19,15 +19,15 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationWillTerminate(_: Notification) {
     }
-    
+
     private func setupLogging() {
         var configs = [LogConfiguration]()
-        
+
         // create a recorder for logging to stdout & stderr
         // and add a configuration that references it
         let stderr = StandardStreamsLogRecorder(formatters: [XcodeLogFormatter()])
         configs.append(BasicLogConfiguration(minimumSeverity: .debug, recorders: [stderr]))
-        
+
         // create a recorder for logging via OSLog (if possible)
         // and add a configuration that references it
         if let osLog = OSLogRecorder(formatters: [ReadableLogFormatter()]) {
@@ -35,27 +35,27 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             // a platform that doesn’t support the os_log() function
             configs.append(BasicLogConfiguration(recorders: [osLog]))
         }
-        
+
         let documentsPath = NSURL(fileURLWithPath: NSSearchPathForDirectoriesInDomains(.libraryDirectory, .userDomainMask, true)[0])
         let logsPath = documentsPath.appendingPathComponent("logs")
-        
+
         #if DEBUG
             let minimumSeverity: CleanroomLogger.LogSeverity = .debug
         #else
             let minimumSeverity: CleanroomLogger.LogSeverity = .info
         #endif
-        
+
         // create a configuration for a 15-day rotating log directory
         let fileCfg = RotatingLogFileConfiguration(minimumSeverity: minimumSeverity,
                                                    daysToKeep: 15,
                                                    directoryPath: logsPath!.path,
                                                    formatters: [ReadableLogFormatter()])
-        
+
         // crash if the log directory doesn’t exist yet & can’t be created
         try! fileCfg.createLogDirectory()
-        
+
         configs.append(fileCfg)
-        
+
         // enable logging using the LogRecorders created above
         Log.enable(configuration: configs)
     }
