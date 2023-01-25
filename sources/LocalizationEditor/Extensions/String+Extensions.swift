42f15af730939ee51c4036ea44c501d506d000a7
Morris Richman <81453549+Mcrich23@users.noreply.github.com>
2023-01-24 22:44:26 -0800

    Formatting updates to maintain xcode format

--- sources/LocalizationEditor/Extensions/String+Extensions.swift
+++ sources/LocalizationEditor/Extensions/String+Extensions.swift
@@ -40,4 +40,12 @@ extension String {
     var localized: String {
         return NSLocalizedString(self, tableName: nil, bundle: Bundle.main, value: "", comment: "")
     }
+    
+    func slice(from: String, to: String) -> String? {
+        return (range(of: from)?.upperBound).flatMap { substringFrom in
+            (range(of: to, range: substringFrom..<endIndex)?.lowerBound).map { substringTo in
+                String(self[substringFrom..<substringTo])
+            }
+        }
+    }
 }
